739678acfac5d8648b1577a248da9506ac97a99b
fanigm <164099396+fanigm@users.noreply.github.com>
2024-04-25 00:24:55 +0000

    Proyecto grid + javascript

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-.gradiente {
-    background: rgb(2,0,36);
-    background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(117,21,52,1) 35%, rgba(255,0,138,0.4374124649859944) 100%);
-    height: 400px;
-    width: 675px;
-    border-radius: 5px;
-    grid-column: 3/5;
-    grid-row: 3/5;
-    z-index: -9;
-}
+function cambiarcolor() { 
+    var element = document.getElementById("boton");
+    element.className = (element.className == "gradiente") ? "vargradiente" : "gradiente";
+ }
