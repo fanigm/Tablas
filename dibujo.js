31af75901304aca064e32b515b0db32f812a4cb5
fanigm <164099396+fanigm@users.noreply.github.com>
2024-05-10 00:07:54 +0000

    Intento de ejercicio de insertar el dibujo de JS en el html

---
+++ dibujo.js
@@ -0,0 +1,10 @@
+c.fillStyle="rgb(102,51,0";
+c.fillRect (100,100,200,125);
+
+c.fillStyle="rgb(204,102,0";
+c.fillRect (100,50,45,50);
+
+c.fillStyle="rgb(102,0,0";
+c.fillRect (175,165,45,60);
+c.fillRect (125,145,30,30);
+c.fillRect (250,145,30,30);
